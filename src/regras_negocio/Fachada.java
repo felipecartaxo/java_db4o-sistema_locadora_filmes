c4fa379322fdd57fb08aa04f3d918533f5152fee
Neraky <felipecartaxo1@gmail.com>
2024-06-30 21:08:51 -0300

    Implementação de excluirVideo

--- src/regras_negocio/Fachada.java
+++ src/regras_negocio/Fachada.java
@@ -11,7 +11,9 @@ import daodb4o.DAO;
 import daodb4o.DAOGenero;
 import daodb4o.DAOUsuario;
 import daodb4o.DAOVideo;
+import modelo.Aluguel;
 import modelo.Carro;
+import modelo.Cliente;
 import modelo.Motor;
 import modelo.Motorista;
 import modelo.Usuario;
@@ -47,49 +49,17 @@ public class Fachada {
 		return video;
 	}
 	
-	public static void criarCarro(String placa, String nomemotor, String cnh) throws Exception {
+	public static void excluirVideo(String titulo) throws Exception{
+		
 		DAO.begin();
-		Carro  carro = daocarro.read(placa);
-		if (carro != null) {
-			DAO.rollback();
-			throw new Exception("criar carro - placa ja existe:" + placa);
-		}
-		Motor  motor = daomotor.read(nomemotor);
-		if (motor == null) {
-			DAO.rollback();
-			throw new Exception("criar carro - motor nao existe:" + nomemotor);
-		}
-		Motorista  motorista = daomotorista.read(cnh);
-		if (motorista == null) {
-			DAO.rollback();
-			throw new Exception("criar carro - motorista nao existe:" + cnh);
-		}
-		carro = new Carro(placa, motor, motorista);
-		daocarro.create(carro);
+		Video video = daovideo.read(titulo);
+		if(video == null) 
+			throw new Exception ("video incorreto para exclusao " + titulo);
+		
+		// Apaga o vídeo sem apagar os gêneros, pois podem existir outros filmes que compartilham do mesmo gênero
+		daovideo.delete(video);
 		DAO.commit();
 	}
-
-	public static void alterarMotoristaDoCarro(String placa, String cnh) throws Exception {
-		DAO.begin();
-		Carro  carro = daocarro.read(placa);
-		if (carro == null) {
-			DAO.rollback();
-			throw new Exception("alterar motorista do carro - placa inexistente:" + placa);
-		}
-		Motorista  motorista = daomotorista.read(cnh);
-		if (motorista == null) {
-			DAO.rollback();
-			throw new Exception("alterar motorista do carro - motorista inexistente:" + cnh);
-		}
-		if (motorista.equals(carro.getMotorista())) {
-			DAO.rollback();
-			throw new Exception("alterar motorista do carro - motorista nao pode ser o atual:" + cnh);
-		}
-		carro.setMotorista(motorista);
-		daocarro.update(carro);
-		DAO.commit();
-	}
-	
 	
 	public static List<Carro> listarCarros() {
 		List<Carro> result = daocarro.readAll();
